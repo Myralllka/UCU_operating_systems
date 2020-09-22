67757f14d20c5de4eb1ba407123b5a6956f7268b
Mykola Morgunenko <Olli25010@gmail.com>
2020-09-22 16:41:07 +0300

    reserve

---
+++ lab1_bash_script/file1.h
@@ -0,0 +1,8 @@
+//
+// Created by myralllka on 9/17/20.
+//
+
+#ifndef LAB1_BASH_SCRIPT_FILE1_H
+#define LAB1_BASH_SCRIPT_FILE1_H
+
+#endif //LAB1_BASH_SCRIPT_FILE1_H
---
+++ lab1_bash_script/file2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by myralllka on 9/17/20.
+//
+
+#include "file2.h"
